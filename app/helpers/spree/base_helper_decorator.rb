4f7d5e393f7e91b5a1a0cf62fbada19e459117ac
Zack Siri <zack@codemy.net>
2016-01-26 12:27:16 +0700

    customized logo helper

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,5 +1,5 @@
 module Spree::BaseHelper
-  def logo(image_path=Spree::Config[:logo], img_options: {})
-  	link_to image_tag(image_path, img_options), spree.root_path
+  def logo(image_path=Spree::Config[:logo], img_options: {}, link_options: {})
+  	link_to image_tag(image_path, img_options), spree.root_path, link_options
   end
 end
